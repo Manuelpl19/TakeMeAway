33ff3e9c9e41a0e589aeeac562ddcd113f532018
Manuelpl19 <manuelperea10@gmail.com>
2025-12-22 23:19:47 +0100

    Fix CORS: permitir acceso a Netlify

--- tfg/config/cors.php
+++ tfg/config/cors.php
@@ -16,6 +16,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://takemyaway.netlify.app/'
     ],
 
     'allowed_origins_patterns' => [],
@@ -26,6 +27,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
